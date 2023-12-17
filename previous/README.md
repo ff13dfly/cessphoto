457155034d0c2b9beec18bd5a12f388d02a03c5f
fuu <fu@yihaofang.com>
2023-12-17 08:35:44 +0800

    Isolated from W3OS github

---
+++ previous/README.md
@@ -0,0 +1,9 @@
+# Chain Photo
+
+This is a project for Polkadot Hackson Winter 2023, bounty of CESS.
+
+## Overview
+
+## Account
+
+## Data
---
+++ previous/src/App.js
@@ -0,0 +1,14 @@
+import Nav from "./components/header";
+import Body from "./components/body";
+import Upload from "./components/upload";
+
+function App() {
+  
+  return (<div>
+    <Nav />
+    <Body />
+    <Upload />
+  </div>);
+}
+
+export default App;
